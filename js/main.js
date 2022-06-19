1e2c7b9357726602926898d703a670fe92328b61
Tapas Kumar Sahu <tapassahu04@gmail.com>
2022-06-19 22:52:47 +0530

    complete project uploaded

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+    // console.dir(document.documentElement.scrollTop);
+})
+
+// nav hide
+let collapse = document.querySelector(".navbar-collapse.collapse");
+let navItems = document.querySelectorAll(".nav-link");
+for(let item of navItems){
+    item.addEventListener("click",()=>{
+        collapse.classList.remove("show");
+    })
+}
+
+// Owl Carousel
+$(document).ready(function() {
+    $(".client-slider-section").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+});
+
+// project counter
+
